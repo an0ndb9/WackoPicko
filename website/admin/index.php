beef707ffa244c9db40a6068a0f15b75aff6fbd2
Saraunsh Shewale <47555889+an0ndb9@users.noreply.github.com>
2023-04-27 13:38:47 +0100

    Update index.php
    Unauthorized File Inclusion vulnerability has been patched!

--- website/admin/index.php
+++ website/admin/index.php
@@ -1,5 +1,16 @@
 <?php
 
+$allowed_pages = array('login', 'index', 'home'); // Whitelisting web pages!
+
+$page = isset($_GET['page']) ? $_GET['page'] : 'login'; // Default to login page if 'page' parameter is not set
+
+if (!in_array($page, $allowed_pages)) {
+
+    // Invalid page requested, show an error or redirect to default page
+    header('Location: /admin/login.php');
+    exit();
+}
+
 $page = $_GET['page'] . '.php';
 require_once($page);
 
