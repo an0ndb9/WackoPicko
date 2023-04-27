96b6f24d00bd6a90ad7d8ed3324581ffe74dc458
Saraunsh Shewale <47555889+an0ndb9@users.noreply.github.com>
2023-04-27 23:28:22 +0100

    Update upload.php
    Sanitized user controllable input - tag field!

--- website/pictures/upload.php
+++ website/pictures/upload.php
@@ -22,49 +22,58 @@ if (isset($_POST['tag']) && isset($_POST['name']) && isset($_FILES['pic']) && is
       $_POST['name'] = str_replace("..", "", $_POST['name']);
       $_POST['name'] = str_replace(" ", "", $_POST['name']);
       $_POST['name'] = str_replace("/", "", $_POST['name']);
+
+      $_POST['tag'] = preg_replace('/[^a-zA-Z0-9_]/', '', $_POST['tag']); // strip all except alphabets
+      $_POST['tag'] = str_replace('..', '', $_POST['tag']); // remove dot dot ".."
+      $_POST['tag'] = str_replace("/", "", $_POST['tag']); // replace "/" with "space"
+
       if (!file_exists("../upload/{$_POST['tag']}/"))
       {
-	 mkdir("../upload/{$_POST['tag']}", 0777, True);
+         mkdir("../upload/{$_POST['tag']}", 0777, True);
       }
       $filename = "../upload/{$_POST['tag']}/{$_POST['name']}";
       $relfilename = "{$_POST['tag']}/{$_POST['name']}";
       if ($_POST['price'] < 0)
       {
-	 $_POST['price'] = abs($_POST['price']);
+         $_POST['price'] = abs($_POST['price']);
       }
       if (file_exists($filename))
       {
-	 $new_name = tempnam("../upload", $filename);
-	 move_uploaded_file($_FILES['pic']['tmp_name'], $new_name);
-	 $id = Pictures::add_conflict($filename, $new_name, $_POST['tag'], $_POST['title'], $_POST['price'], $user['id']);
-	 http_redirect(Pictures::$CONFLICT_URL . "?conflictid={$id}");
+         $new_name = tempnam("../upload", $filename);
+         move_uploaded_file($_FILES['pic']['tmp_name'], $new_name);
+         $id = Pictures::add_conflict($filename, $new_name, $_POST['tag'], $_POST['title'], $_POST['price'], $user['id']);
+         http_redirect(Pictures::$CONFLICT_URL . "?conflictid={$id}");
       }
       else
       {
-	 if (move_uploaded_file($_FILES['pic']['tmp_name'], $filename))
-	 {
-	    
-	    if ($id = Pictures::create_picture($_POST['title'], 128, 128, $_POST['tag'], $relfilename, $_POST['price'], $user['id']))
-	    {
-	       $main = ".550.jpg";
-	       $side = ".128.jpg";
-	       $thumb= ".128_128.jpg";
-	       Pictures::resize_image($filename, $filename . $main, 550, 10000000);
-	       Pictures::resize_image($filename, $filename . $side, 128, 10000000);
-	       Pictures::resize_image($filename, $filename . $thumb, 128, 128);
-	       
-	       http_redirect(Pictures::$VIEW_PIC_URL . "?picid={$id}");
-	       $file_uploaded = True;
-	    }
-	    else
-	    {
-	       $flash['error'] = "Couldn't create your picture, something wrong with the database";
-	    }
-	 }
-	 else
-	 {
-	    $flash['error'] = "Couldn't move picture";
-	 }
+         if (move_uploaded_file($_FILES['pic']['tmp_name'], $filename))
+         {
+            if ($id = Pictures::create_picture($_POST['title'], 128, 128, $_POST['tag'], $relfilename, $_POST['price'], $user['id']))
+            {
+               $main = ".550.jpg";
+               $side = ".128.jpg";
+               $thumb= ".128_128.jpg";
+               Pictures::resize_image($filename, $filename . $main, 550, 10000000);
+               Pictures::resize_image($filename, $filename . $side, 128, 10000000);
+               Pictures::resize_image($filename, $filename . $thumb, 128, 128);
+
+               unlink($new_name);
+
+               http_redirect(Pictures::$VIEW_PIC_URL . "?picid={$id}");
+               $file_uploaded = True;
+            }
+            else
+            {
+               $flash['error'] = "Couldn't create your picture, something wrong with the database";
+            }
+         }
+         else
+         {
+            $flash['error'] = "Couldn't move picture";
+         }
+
+         //delete temporary files!
+         unlink($_FILES['pic']['tmp_name']);
       }
    }
 }
