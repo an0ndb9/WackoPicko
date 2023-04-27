fc51681ea265b9d293263b0af6607b6a92c28a5a
Saraunsh Shewale <47555889+an0ndb9@users.noreply.github.com>
2023-04-27 22:17:18 +0100

    Update index.php
    Fixed minor issues & added preg_match() function to sanitize the input!

--- website/admin/index.php
+++ website/admin/index.php
@@ -5,13 +5,18 @@ $allowed_pages = array('login', 'index', 'home'); // Whitelisting web pages!
 $page = isset($_GET['page']) ? $_GET['page'] : 'login'; // Default to login page if 'page' parameter is not set
 
 if (!in_array($page, $allowed_pages)) {
-
+    
     // Invalid page requested, show an error or redirect to default page
-    header('Location: /admin/login.php');
+    header('Location: login.php');
     exit();
 }
 
-$page = $_GET['page'] . '.php';
+// Sanitizing "page" parameter
+$page = preg_replace("/[^a-zA-Z]/", "", $page);
+$page = str_replace("..", "", $page); // Remove dot dots
+$page = $page . '.php';
+
 require_once($page);
 
 ?>
+
