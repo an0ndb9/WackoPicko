d2d251dbf997a94b517526abd76ceb804e4ece03
Saraunsh Shewale <47555889+an0ndb9@users.noreply.github.com>
2023-04-29 17:58:25 +0100

    Update preview_comment.php
    Added htmlspecialchars() function!

--- website/comments/preview_comment.php
+++ website/comments/preview_comment.php
@@ -6,12 +6,12 @@ require_once("../include/functions.php");
 require_once("../include/pictures.php");
 
 session_start();
-
 require_login();
 
 $error = False;
 $previewid = 0;
 $pic = 0;
+
 if (isset($_POST['text']) && isset($_POST['picid']))
 {
    $cur = Users::current_user();
@@ -23,11 +23,11 @@ if (isset($_POST['text']) && isset($_POST['picid']))
    {
       if (!($pic = Pictures::get_picture($_POST['picid'])))
       {
-	 $error = True;
+         $error = True;
       }
       else
       {
-	 $error = False;
+         $error = False;
       }
    }
 }
@@ -56,13 +56,13 @@ if ($error)
 <div class="column prepend-1 span-14 first" >
   <h2 id="image-title">A Preview of what your comment on <?=h( $pic['title'] )?> will look like </h2>
   <img id="image" src="../upload/<?=h( $pic['filename'] )?>.550.jpg" width="550" />
-	
+
   <div class="column span-14 first last " id="comments">
     <div class="column span-14 first last">
       <h2 id="comment-title">Your Comment</h2>
     </div>
     <div class="column prepend-1 span-12 first last">
-      <p class="comment"><?=h( $_POST['text'] )?></p>
+      <p class="comment"><?=h(htmlspecialchars($_POST['text']))?></p>
     </div>
     <div class="column prepend-10 span-6 first last">
       - by <a href="<?= Users::$VIEW_URL ?>?userid=<?=h( $cur['id'] )?>"><?=h( $cur['login'] ) ?></a>
@@ -79,8 +79,5 @@ if ($error)
   </form>
   </div>
 </div>
-
-
-
 
 <?php our_footer(); ?>
