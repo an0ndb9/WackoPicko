95d408db4c20616dc0d4b4f594eb7f2ce8e1203e
Saraunsh Shewale <47555889+an0ndb9@users.noreply.github.com>
2023-04-29 23:41:53 +0100

    Update search.php
    Minor changes!

--- website/pictures/search.php
+++ website/pictures/search.php
@@ -7,23 +7,23 @@ require_once("../include/functions.php");
 
 session_start();
 
-/*if (!isset($_GET['query']))
-{
-   http_redirect("/error.php?msg=Error, need to provide a query to search");
-}*/
-
-$pictures = Pictures::get_all_pictures_by_tag($_GET['query']);
 if (!isset($_GET['query']) || !preg_match('/^[a-zA-Z0-9]+$/', $_GET['query']))
 {
    http_redirect("/error.php?msg=Error, invalid query provided");
    exit();
 }
+
+$pictures = Pictures::get_all_pictures_by_tag($_GET['query']);
+
+#Patched code for reflected XSS ( inserted htmlspecialchars tag to prevent malicious inputs )
+$query = htmlspecialchars($_GET['query'], ENT_QUOTES, 'UTF-8');
+
 ?>
 
 <?php our_header("", $_GET['query']); ?>
 
 <div class="column prepend-1 span-24 first last">
-<h2>Pictures that are tagged as '<?= $_GET['query']  ?>'</h2>
+<h2>Pictures that are tagged as '<?= htmlspecialchars($query) ?>'</h2>
 
    <?php thumbnail_pic_list($pictures); ?>
 
