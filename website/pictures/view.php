d546719612b7cbba496974b0fe445bdc76d9cfdc
Saraunsh Shewale <47555889+an0ndb9@users.noreply.github.com>
2023-04-29 17:53:34 +0100

    Update view.php
    Fixed missing output sanitization!

--- website/pictures/view.php
+++ website/pictures/view.php
@@ -41,27 +41,27 @@ if ($no_pic)
 
 <div class="column prepend-1 span-14 first" >
    <h2 id="image-title"><?=h( $pic['title'] )?> </h2>
-	<img id="image" src="../upload/<?=h( $pic['filename'] )?>.550.jpg" width="550" />
-	
-	<div class="column span-14 first last " id="comments">
-	  <div class="column span-14 first last">
-	    <h2 id="comment-title">Comments</h2>
-	  </div>
+        <img id="image" src="../upload/<?=h( $pic['filename'] )?>.550.jpg" width="550" />
+
+        <div class="column span-14 first last " id="comments">
+          <div class="column span-14 first last">
+            <h2 id="comment-title">Comments</h2>
+          </div>
    <?php if ($comments) {
    foreach ($comments as $comment) {  ?>
-	  <div class="column prepend-1 span-12 first last">
-	    <p class="comment"><?= $comment['text'] ?></p>
-	  </div>
-	  <div class="column prepend-10 span-6 first last">
-	  - by <a href="<?= Users::$VIEW_URL ?>?userid=<?=h( $comment['user_id'] )?>"><?=h( $comment['login'] ) ?></a>
-	  </div>
+          <div class="column prepend-1 span-12 first last">
+            <p class="comment"><?=filter_var( $comment['text'],FILTER_SANITIZE_STRING) ?></p>
+          </div>
+          <div class="column prepend-10 span-6 first last">
+          - by <a href="<?= Users::$VIEW_URL ?>?userid=<?=h( $comment['user_id'] )?>"><?=h( $comment['login'] ) ?></a>
+          </div>
 <?php
    }
 }
 
 else
 {
-?>	  
+?>        
    <div class="column prepend-1-span12 first last" style="padding-top:2em;padding-bottom:2em">
       <h3 style="text-align:center">No comments yet...</h3>
       </div>
@@ -69,59 +69,57 @@ else
       }
 
 ?>
-	  <div class="column prepend-1 span-12 first last" style="padding-bottom:2em;">
-	    <h3 style="text-align:center;padding-top:2em;">Add your comment</h3>
-	    <form action="<?= Comments::$PREVIEW_COMMENT_URL ?>" method="POST">
-	      <textarea id="comment-box" name="text"></textarea>
-	      <div class="column prepend-9 first last">
-		<input type="hidden" name="picid" value="<?=h( $pic['id'] )?>"/>
-		<input type="submit" value="Preview"/>
+          <div class="column prepend-1 span-12 first last" style="padding-bottom:2em;">
+            <h3 style="text-align:center;padding-top:2em;">Add your comment</h3>
+            <form action="<?= Comments::$PREVIEW_COMMENT_URL ?>" method="POST">
+              <textarea id="comment-box" name="text"></textarea>
+              <div class="column prepend-9 first last">
+                <input type="hidden" name="picid" value="<?=h( $pic['id'] )?>"/>
+                <input type="submit" value="Preview"/>
 
-	      </div>
-	    </form>
-	  </div>
-	  
-	</div>
+              </div>
+            </form>
+          </div>
+
+        </div>
       </div>
-      
+
       <div class="column prepend-2 append-1 span-6 last" style="padding-top:1em;" >
    <h3 id="tradebux"><?=h( $pic['price'] )?> Tradebux<br />
    <?php $usr = Users::current_user(); if ($usr['id'] != $pic['user_id']) { ?>
-	  <a href="<?=h( Cart::$ACTION_URL . '?action=add&picid=' . $pic['id'] );?>">Add to Cart</a>
+          <a href="<?=h( Cart::$ACTION_URL . '?action=add&picid=' . $pic['id'] );?>">Add to Cart</a>
     <?php } ?>
-	</h3>
-	<h3 id="info-area">Uploaded on <?=h( date("F j, Y", $pic['created_on_unix']) )?><br />
-					  by <a href="<?= Users::$VIEW_URL ?>?userid=<?=h( $pic['user_id'] ) ?>"><?=h($pic['login']) ?></a>
-	</h3>  
-	<?php if ($related) { ?>					  
-	<div class="column span-6 first last" id="related">
-	  <div class="column span-6 first last">
-	    <h2 id="related-title">Related</h2>
-	  </div>
+        </h3>
+        <h3 id="info-area">Uploaded on <?=h( date("F j, Y", $pic['created_on_unix']) )?><br />
+                                          by <a href="<?= Users::$VIEW_URL ?>?userid=<?=h( $pic['user_id'] ) ?>"><?=h($pic['login']) ?></a>
+        </h3>  
+        <?php if ($related) { ?>
+        <div class="column span-6 first last" id="related">
+          <div class="column span-6 first last">
+            <h2 id="related-title">Related</h2>
+          </div>
       <?php foreach ($related as $p) { ?>
-	  <div class="column prepend-1 span-4 first last">
-	    <a href="<?=h( Pictures::$VIEW_PIC_URL . "?picid=" . $p['id'] ) ?>"><img src="/upload/<?=h($p['filename']) ?>.128.jpg" width="128" /></a>
-	 <p>by <a href="<?= Users::$VIEW_URL ?>?userid=<?=h( $p['user_id'] )?>"><?=h( $p['login'] )?></a></p>
-	  </div>
+          <div class="column prepend-1 span-4 first last">
+            <a href="<?=h( Pictures::$VIEW_PIC_URL . "?picid=" . $p['id'] ) ?>"><img src="/upload/<?=h($p['filename']) ?>.128.jpg" width="128" /></a>
+         <p>by <a href="<?= Users::$VIEW_URL ?>?userid=<?=h( $p['user_id'] )?>"><?=h( $p['login'] )?></a></p>
+          </div>
 <?php }?>
-	</div>
-	<?php } ?>
-	<?php if ($same) { ?>
-	<div class="column span-6 first last" id="same-upload">
-	  <div class="column span-6 first last">
-	    <h2 id="same-upload-title">Other by <a href="<?= Users::$VIEW_URL ?>?userid=<?=h( $pic['user_id'] )?>"><?=h($pic['login']) ?></a></h2>
-	  </div>
+        </div>
+        <?php } ?>
+        <?php if ($same) { ?>
+        <div class="column span-6 first last" id="same-upload">
+          <div class="column span-6 first last">
+            <h2 id="same-upload-title">Other by <a href="<?= Users::$VIEW_URL ?>?userid=<?=h( $pic['user_id'] )?>"><?=h($pic['login']) ?></a></h2>
+          </div>
 
       <?php foreach ($same as $pic) { ?>
-	  <div class="column prepend-1 span-4 first last" style="margin-bottom:2em;">
-	    <a href="<?=h( Pictures::$VIEW_PIC_URL . "?picid=" . $pic['id'] ) ?>"><img src="/upload/<?=h($pic['filename']) ?>" width="128" /></a>
-	  </div>
+          <div class="column prepend-1 span-4 first last" style="margin-bottom:2em;">
+            <a href="<?=h( Pictures::$VIEW_PIC_URL . "?picid=" . $pic['id'] ) ?>"><img src="/upload/<?=h($pic['filename']) ?>" width="128" /></a>
+          </div>
 <?php }?>
-	</div>
-	<?php } ?>
-	
+        </div>
+        <?php } ?>
+        
       </div>
-
-
 
 <?php our_footer(); ?>
